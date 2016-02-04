ad9349b81fed11ac12440c671e9ece6eaffa8e41
sebastianschnock <sebastian.schnock@gmail.com>
2016-02-04 13:59:36 +0100

    Small usability tweaks

--- src/js/app.js
+++ src/js/app.js
@@ -3,20 +3,27 @@ import getBaseDomain from './get-base-domain';
 (() => {
 
 	let inputElem = document.querySelector('.get-base-domain__input');
+	let formElem = document.querySelector('.get-base-domain__form');
 	let executeElem = document.querySelector('.get-base-domain__execute-search');
 	let resultElem = document.querySelector('.get-base-domain__result');
 	let durationElem = document.querySelector('.get-base-domain__duration');
 
-	executeElem.addEventListener('click', () => {
+	let preventSubmit = false;
+
+	formElem.addEventListener('submit', (event) => {
+		event.preventDefault();
+		if(preventSubmit) return false;
 		const t1 = window.performance.now();
 		const url = inputElem.value;
 		executeElem.disabled = true;
+		preventSubmit = true;
 		getBaseDomain(url, (baseDomain) => {
 			const t2 = window.performance.now();
 			const duration = (t2-t1).toFixed(2);
 			resultElem.textContent = `The base domain of ${url} is ${baseDomain}`;
 			durationElem.textContent = `executed in ${duration}ms`
 			executeElem.disabled = false;
+			preventSubmit = false;
 		});
 	});
 
